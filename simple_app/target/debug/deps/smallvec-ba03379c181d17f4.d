69cb0b1d904c25de2fccd58a67de3c9d4777dafe
Zhu Hengbo <hengbo@nj.iscas.ac.cn>
2024-08-15 17:46:43 +0800

    WIP
    Signed-off-by: Zhu Hengbo <hengbo@nj.iscas.ac.cn>

--- simple_app/target/debug/deps/smallvec-ba03379c181d17f4.d
+++
@@ -1,7 +0,0 @@
-/Users/zhuhengbo/Documents/3_web3/rust_practice/simple_app/target/debug/deps/libsmallvec-ba03379c181d17f4.rmeta: /Users/zhuhengbo/.cargo/registry/src/index.crates.io-6f17d22bba15001f/smallvec-1.13.2/src/lib.rs
-
-/Users/zhuhengbo/Documents/3_web3/rust_practice/simple_app/target/debug/deps/libsmallvec-ba03379c181d17f4.rlib: /Users/zhuhengbo/.cargo/registry/src/index.crates.io-6f17d22bba15001f/smallvec-1.13.2/src/lib.rs
-
-/Users/zhuhengbo/Documents/3_web3/rust_practice/simple_app/target/debug/deps/smallvec-ba03379c181d17f4.d: /Users/zhuhengbo/.cargo/registry/src/index.crates.io-6f17d22bba15001f/smallvec-1.13.2/src/lib.rs
-
-/Users/zhuhengbo/.cargo/registry/src/index.crates.io-6f17d22bba15001f/smallvec-1.13.2/src/lib.rs:
--- simple_app/target/debug/simple_app.d
+++
@@ -1,1 +0,0 @@
-/Users/zhuhengbo/Documents/3_web3/rust_practice/simple_app/target/debug/simple_app: /Users/zhuhengbo/Documents/3_web3/rust_practice/simple_app/src/main.rs
